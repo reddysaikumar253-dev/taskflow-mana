b78eeecf4580bf9b8fdce55ecdb409ef2fd13b87
SAIKUMAR REDDY <reddysaikumar253@gmail.com>
2025-12-06 18:09:42 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -49,4 +49,8 @@ registerRoutes(httpServer, app);
 const port = parseInt(process.env.PORT || "5000", 10);
 httpServer.listen(port, () => {
   console.log(`Server running on port ${port}`);
+  app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
+
 });
