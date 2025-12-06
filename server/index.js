0b3a43565e42f050852690c96d9f2cf157a66112
SAIKUMAR REDDY <reddysaikumar253@gmail.com>
2025-12-06 18:17:05 +0530

    Backend-Fix

--- server/index.js
+++ server/index.js
@@ -50,7 +50,7 @@ const port = parseInt(process.env.PORT || "5000", 10);
 httpServer.listen(port, () => {
   console.log(`Server running on port ${port}`);
   app.get("/", (req, res) => {
-  res.send("Server is running 🚀");
+  res.send("Server is running ");
 });
 
 });
