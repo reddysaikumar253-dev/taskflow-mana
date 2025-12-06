6235b60b117a48ebef74e358db4a5124078ace9c
SAIKUMAR REDDY <reddysaikumar253@gmail.com>
2025-12-06 17:37:44 +0530

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      base: process.env.VITE_BASE_PATH || "/taskflow-mana",
     },
   },
 });
